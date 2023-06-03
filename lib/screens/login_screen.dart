c49e1bbe5878aaeffd11b9a6d7956f6874d0961b
lmuchapirei <lmuchapirei@cdp.dental>
2023-06-03 15:18:22 +0200

    Configure Google SIgn In

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,17 +1,25 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:docs_clone/colors.dart';
+import 'package:docs_clone/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.watch(authRepositoryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-            onPressed: () {},
+            onPressed: () {
+              signInWithGoogle(ref);
+            },
             icon: Image.asset(
               "assets/images/g-logo-2.png",
               height: 20,
